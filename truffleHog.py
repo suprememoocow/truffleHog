1b8b1426d642979ed88a4b6369ccf5df18b0a8c5
jguasch <jguasch@yammer-inc.com>
2017-01-03 18:21:04 +0000

    added support for JSON output

--- truffleHog.py
+++ truffleHog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import shutil, sys, math, string, datetime, argparse, tempfile, os, fnmatch
 from git import Repo
+import json
 
 if sys.version_info[0] == 2:
     reload(sys)  
@@ -60,13 +61,14 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def find_strings(git_url):
+def find_strings(git_url, output):
     project_path = tempfile.mkdtemp()
 
     Repo.clone_from(git_url, project_path)
 
     repo = Repo(project_path)
 
+    jsonOutput = output
 
     already_searched = set()
     for remote_branch in repo.remotes.origin.fetch():
@@ -110,25 +112,42 @@ def find_strings(git_url):
                                 b64Entropy = shannon_entropy(string, BASE64_CHARS)
                                 if b64Entropy > 4.5:
                                     foundSomething = True
+                                    if jsonOutput:
+                                        stringDiff = string
+                                    else:
                                     printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
                             for string in hex_strings:
                                 hexEntropy = shannon_entropy(string, HEX_CHARS)
                                 if hexEntropy > 3:
                                     foundSomething = True
+                                    if jsonOutput:
+                                        stringDiff = string
+                                    else:
                                     printableDiff = printableDiff.replace(string, bcolors.WARNING + string + bcolors.ENDC)
                     if foundSomething:
                         commit_time =  datetime.datetime.fromtimestamp(prev_commit.committed_date).strftime('%Y-%m-%d %H:%M:%S')
-                        print(bcolors.OKGREEN + "File: " + str(path) + bcolors.ENDC)
-                        print(bcolors.OKGREEN + "Date: " + commit_time + bcolors.ENDC)
-                        print(bcolors.OKGREEN + "Branch: " + branch_name + bcolors.ENDC)
-                        print(bcolors.OKGREEN + "Commit: " + prev_commit.message + bcolors.ENDC)
-                        print(printableDiff)
+                        if jsonOutput:
+                            output = {}
+                            output['file'] = str(path)
+                            output['date'] = commit_time
+                            output['branch'] = branch_name
+                            output['commit'] = prev_commit.message
+                            output['diff'] = printableDiff
+                            output['string'] = stringDiff
+                            print json.dumps(output)
+                        else:
+                            print(bcolors.OKGREEN + "File: " + str(path) + bcolors.ENDC)
+                            print(bcolors.OKGREEN + "Date: " + commit_time + bcolors.ENDC)
+                            print(bcolors.OKGREEN + "Branch: " + branch_name + bcolors.ENDC)
+                            print(bcolors.OKGREEN + "Commit: " + prev_commit.message + bcolors.ENDC)
+                            print(printableDiff)
                     
             prev_commit = curr_commit
     shutil.rmtree(project_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find secrets hidden in the depths of git.')
+    parser.add_argument('--json', dest="output_json", action="store_true", help="Output in JSON")
     parser.add_argument('git_url', type=str, help='URL for secret searching')
 
     # if the .fileignore file exists, attempt to import file patterns
@@ -141,5 +160,4 @@ if __name__ == "__main__":
         pass
 
     args = parser.parse_args()
-    find_strings(args.git_url)
-
+    find_strings(args.git_url, args.output_json)
